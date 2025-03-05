9b2ba58061e1ca4d539790d2294aa089f0613836
Edvard Haugland <eddiehaug@users.noreply.github.com>
2025-03-05 21:29:41 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "qwiklabs-gcp-03-0c5b138c6d2f-bucket-tfstate" # Use your bucket name here
+    bucket = "value" # Use your bucket name here
     # prefix = "terraform/state" # Optional: Organize states in a prefix
   }
 }
